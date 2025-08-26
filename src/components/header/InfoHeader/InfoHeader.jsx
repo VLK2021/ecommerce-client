925857c320264b9bc605586ef971baef458cac15
vlk2022 <volodymyrkostiuk2021@gmail.com>
2025-08-26 22:13:36 +0300

    commit

--- src/components/header/InfoHeader/InfoHeader.jsx
+++ src/components/header/InfoHeader/InfoHeader.jsx
@@ -6,7 +6,25 @@ import css from './InfoHeader.module.css';
 const InfoHeader = () => {
     return (
         <div className={css.wrap}>
-            InfoHeader
+            <div>
+                VV-SHOP
+            </div>
+
+            <div>
+                search
+            </div>
+
+            <div>
+                time work
+            </div>
+
+            <div>
+                icons
+            </div>
+
+            <div>
+                like and card
+            </div>
         </div>
     );
 };
